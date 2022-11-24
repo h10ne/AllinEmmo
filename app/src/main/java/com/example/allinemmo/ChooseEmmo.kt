889618f41ac9fb5e6b424c5db83a282bca2dba3f
Denis <den4ik-tv@yandex.ru>
2022-11-24 12:15:02 +0300

    добавил анимации появления кружка эмоций

--- app/src/main/java/com/example/allinemmo/ChooseEmmo.kt
+++ app/src/main/java/com/example/allinemmo/ChooseEmmo.kt
@@ -3,6 +3,8 @@ package com.example.allinemmo
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.allinemmo.OneItemsClasses.Emmotion
@@ -25,6 +27,9 @@ class ChooseEmmo : AppCompatActivity() {
         setContentView(R.layout.activity_choose_emmo)
         supportActionBar?.hide()
 
+        val zoomIn = AnimationUtils.loadAnimation(this, R.anim.zoomin)
+        val zoomOut = AnimationUtils.loadAnimation(this, R.anim.zoomout)
+
         emmo1 = findViewById(R.id.emmo_angry_circle)
         emmo2 = findViewById(R.id.emmo_haha_circle)
         emmo3 = findViewById(R.id.emmo_happy_circle)
@@ -33,6 +38,27 @@ class ChooseEmmo : AppCompatActivity() {
         emmo6 = findViewById(R.id.emmo_normal_circle)
         emmo7 = findViewById(R.id.emmo_tears_circle)
         emmo8 = findViewById(R.id.emmo_wow_sad_circle)
+
+        emmo1.startAnimation(zoomOut)
+        emmo2.startAnimation(zoomOut)
+        emmo3.startAnimation(zoomOut)
+        emmo4.startAnimation(zoomOut)
+        emmo5.startAnimation(zoomOut)
+        emmo6.startAnimation(zoomOut)
+        emmo7.startAnimation(zoomOut)
+        emmo8.startAnimation(zoomOut)
+
+        Handler().postDelayed({
+            emmo1.startAnimation(zoomIn)
+            emmo2.startAnimation(zoomIn)
+            emmo3.startAnimation(zoomIn)
+            emmo4.startAnimation(zoomIn)
+            emmo5.startAnimation(zoomIn)
+            emmo6.startAnimation(zoomIn)
+            emmo7.startAnimation(zoomIn)
+            emmo8.startAnimation(zoomIn)
+        }, 100)
+
         emmo = intent.extras?.get("emmo") as Emmotion
 
         setOnItemClick()
